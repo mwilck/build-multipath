f8f060b922975a96430b1dced0e14864f249cb20
Martin Wilck <mwilck@suse.com>
2021-02-11 19:27:59 +0100

    fixup! sle12-sp5: add buildah.sh

--- Docker/sle12-sp5/buildah.sh
+++ Docker/sle12-sp5/buildah.sh
@@ -2,7 +2,7 @@
 trap 'echo error in "$BASH_COMMAND" >&2; exit 1' ERR
 NAME=$1
 [[ $NAME && -f SUSEConnect && -f SCCcredentials ]]
-BASE="registry.suse.com/suse/sle12sp5:6.5.1"
+BASE="registry.suse.com/suse/sle12sp5:6.5.11"
 ADDONS="sle-sdk,PackageHub"
 set -x
 WORK=$(buildah from "$BASE")
