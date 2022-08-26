a51f15b2fe6c6dba14af5526214b245766a13d85
Martin Wilck <mwilck@suse.com>
2022-08-26 09:52:09 +0200

    sle12-sp5: use updated image and cmocka
    wget fails in the old image because it doesn't support TLS 1.3.

--- Docker/sle12-sp5/buildah.sh
+++ Docker/sle12-sp5/buildah.sh
@@ -2,7 +2,7 @@
 trap 'echo error in "$BASH_COMMAND" >&2; exit 1' ERR
 NAME=$1
 [[ $NAME && -f SUSEConnect && -f SCCcredentials ]]
-BASE="registry.suse.com/suse/sles12sp5:6.5.11"
+BASE="registry.suse.com/suse/sles12sp5:6.5.366"
 ADDONS="sle-sdk,PackageHub"
 set -x
 WORK=$(buildah from "$BASE")
@@ -28,9 +28,9 @@ buildah  run --mount=type=bind,src=$PWD/SUSEConnect,dst=/run/secrets/SUSEConnect
 buildah config --env "ADDITIONAL_MODULES-" "$WORK"
 buildah config --volume /build "$WORK"
 buildah config --workingdir /build "$WORK"
-buildah run "$WORK" wget -q https://cmocka.org/files/1.1/cmocka-1.1.1.tar.xz
-buildah run "$WORK" tar xfvJ cmocka-1.1.1.tar.xz
-buildah config --workingdir /build/cmocka-1.1.1/build "$WORK"
+buildah run "$WORK" wget -q https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz
+buildah run "$WORK" tar xfvJ cmocka-1.1.5.tar.xz
+buildah config --workingdir /build/cmocka-1.1.5/build "$WORK"
 buildah run "$WORK" cmake -DCMAKE_INSTALL_PREFIX=/usr \
     -DCMAKE_BUILD_TYPE=Release \
     -DLIB_INSTALL_DIR:PATH=/usr/lib64 \
@@ -38,11 +38,11 @@ buildah run "$WORK" cmake -DCMAKE_INSTALL_PREFIX=/usr \
 buildah run "$WORK" make
 buildah run "$WORK" make install
 buildah config --workingdir /build "$WORK"
-buildah run "$WORK" rm -rf cmocka-1.1.1.tar.xz cmocka-1.1.1
+buildah run "$WORK" rm -rf cmocka-1.1.5.tar.xz cmocka-1.1.5
 buildah run "$WORK" \
 	 zypper -n --no-refresh --no-remote remove \
 	 libidn11 libarchive13  \
-	 libpython2_7-1_0 python-rpm-macros python-base \
+	 libpython2_7-1_0 python-base \
 	 cmake tar
 buildah run "$WORK" zypper clean --all
 buildah config --entrypoint '[ "make" ]' "$WORK"
